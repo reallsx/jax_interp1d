9c1f6b9ed056162fa8f0d067594fe73f9b41c33d
Shixiao Liang <liangsx@rice.edu>
2024-03-22 14:05:21 -0500

    add support for list

--- jinterp.py
+++ jinterp.py
@@ -8,13 +8,18 @@ class interp1d:
     
     '''
     def __init__(self, xp, yp):
-        if xp.shape[-1] != yp.shape[-1]:
-            raise ValueError("x and y arrays must be equal in length along "
-                             "interpolation axis.")
+        if not isinstance(yp, list):
+            if xp.shape[-1] != yp.shape[-1]:
+                raise ValueError("x and y arrays must be equal in length along "
+                                "interpolation axis.")
         self.xp = xp
         self.yp = yp
 
     def __call__(self, x):
-        return jnp.interp(x, self.xp, self.yp)
-    
-
+        if isinstance(self.yp, list):
+            result = []
+            for y in self.yp:
+                result.append(jnp.interp(x, self.xp, y))
+            return jnp.array(result)
+        else:
+            return jnp.interp(x, self.xp, self.yp)
